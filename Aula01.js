aa6042e0d9aead1f0b04a4c4ad3c487c11f489f8
PedroCod <88497687+PedroCod@users.noreply.github.com>
2021-08-18 20:00:56 -0300

    Add files via upload
    Arquivo das duas primeiras aulas

---
+++ Aula01.js
@@ -0,0 +1,108 @@
+// console.log('Olá Mundo!');
+
+// prompt("Digite seu nome: ");
+
+// let usuario = 'Pedro';
+
+// console.log(usuario);
+
+// let aluno = prompt('digite seu nome: ');
+// let turma = prompt("digite sua turma" );
+// console.log('Olá '+aluno+', Seja bem vindo a turma '+turma );
+
+
+
+
+
+//console.log -> exibe uma informação na tela.
+//A sintaxe é: console.log(informacao)
+//No lugar da informação, posso colocar uma string (texto), número, variável, etc...
+
+// console.log('Olá mundo!');
+
+
+
+//prompt -> recebe uma informação passada pelo usuário:
+//Sintaxe: prompt("A frase que vai aparecer na tela")
+
+// prompt('Digite seu nome: ');
+//VARIAVEIS
+//São espaços na memória reservados para guardar uma informação.
+//São como 'potinhos' que damos um nome, guardamos uma informação lá dentro, e quando chamamo essa váriavel pelo nome, podemos receber a informação guardada.
+//Ela pode receber um texto (string), número, etc...
+//Sintaxe: let nomeDaVariavel = valorDaVariavel
+//Sintaxe: var nomeDaVariavel = valorDaVariavel
+
+// let usuario = 'Danusa';
+// console.log(usuario);
+//Recebendo uma informação do usuário e guardando na variável
+
+// let aluno = prompt("Digite seu nome: ");
+// let turma = prompt("Digite sua turma: ");
+
+//Concatenando strings: Juntando textos diferentes para serem exibidos juntos na tela.
+// console.log('Olá '+ aluno +', seja bem vindo(a)à turma '+turma);
+
+
+//variavel Let-var
+
+// let varTexto = "isso é uma string";
+// let varNumero = 30;
+
+// let varBoolean = true;
+
+// console.log(varTexto);
+
+// console.log(varNumero);
+
+// console.log(varBoolean);
+
+
+
+//Variavel Array
+
+// let arrayNome = ["João","Paulo","Jose","Pedro"];
+// let arrayNumero = [100,20,50,30,60];
+// let arrayMisto =["Pedro",false,300];
+
+// console.log(arrayNome[3]);
+// console.log(arrayNumero[0]);
+// console.log(arrayMisto);
+
+//Variaveis
+
+
+// var a = "Turma C7";
+// var b = a, a ;
+// console.log(b);
+
+// var c, d = c = "Nova string";
+// console.log(c);
+// console.log(d);
+
+//const
+
+// // const constTeste = "isso é uma const";
+
+// console.log(constTeste);
+
+//OPERADORES
+
+// let soma = 6 + 9;
+// console.log(soma)
+
+// console.log("Boa"+ " noite");
+
+// let numero1 = 6;
+// let numero2 = 10;
+
+// let somanumeros = numero1 + numero2;
+// console.log(somanumeros);
+// let varMista = "boa" + 10;
+// typeof varMista;
+
+
+// let comparaNumero = 6 !== 3;
+// let comparaTexto = "Texto"!=="Texto";
+// console.log(comparaNumero);
+// console.log(comparaTexto);
