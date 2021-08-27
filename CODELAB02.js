d5819b97f5462d50b583812414688555e4bbfeeb
PedroCod <88497687+PedroCod@users.noreply.github.com>
2021-08-27 10:18:49 -0300

    Add files via upload

---
+++ CODELAB02.js
@@ -0,0 +1,138 @@
+//Exercicio 1
+// console.log("O primeiro programa a gente nunca esquece!");
+
+//exercicio 2
+//  let dados = [nome = prompt("digite seu nome Completo"), endereço = prompt("digite seu endereço"), cepEtelefone = prompt("Digite seu cep e Seu Telefone")];
+
+//  console.log(dados[0]);
+//  console.log(dados[1]);
+//  console.log(dados[2]);
+
+
+//Exercicio3
+// console.log("Yeah \nRest in peace to all the kids that lost their lives in the Parkland shooting \nThis song is dedicated to you \nOkay, she keep cryin', she keep cryin' every single night \nDay and night, on my mind, please don't kill the vibe \nAll alone, swear to God, I be in my mind \nSwear I wanna die, yeah, when you cross my \nSaid I wouldn't die, yeah, no, I'm not alright, yeah \nI might start a riot, I'm so fuckin' tired \nSo, so what you say? \nFeelin' good, I'm feelin' great \nTired of the fuckin' hate, stackin' cheese all on my plate \nSo outside of my misery, I think I'll find \nA way of envisioning a better life For the rest of us, the rest of us \nThere's hope for the rest of us, the rest of us \nOkay, she keep cryin', she keep cryin' every single night \nDay and night, on my mind, please don't kill the vibe \nOh no, I swear to God, I be in my mind \nSwear I wanna die, yeah, when you cross my mind \nSaid I wouldn't die, yeah, no, I'm not alright, yeah \nI might start a riot, I'm so fuckin' tired \nSo, so what you say? Feelin' good, I'm feelin' great \nTired of the fuckin' hate, stackin' cheese all on my plate");
+
+//Exercicio 4
+
+// console.log("ALUNO(A) NOTA");
+// console.log("ALINE    9.0");
+// console.log("MÁRIO    DEZ");
+// console.log("SÉRGIO   4.5");
+// console.log("SHIRLEY  7.0");
+
+
+//Exercicio 5
+// console.log('Cadastro de Clientes');;
+// console.log('0 – Fim');
+// console.log('1 - Inclui');
+// console.log('2 - Altera');
+// console.log('3 - Exclui');
+// console.log('4 - Consulta');
+
+// const opcao = +prompt("Digite uma opção da lista acima: ");
+// console.log(`Você escolheu a opção ${opcao}`);
+
+//Exercicio 6
+
+// console.log("Calculadora de Dano");
+
+// const vida= +prompt(`Digite a quantidade de vidas do monstro (entre 10 e 50) `);
+
+// const ataque= +prompt(`Digite o valor do ataque por turno (entre 5 e 10): `);
+
+// const quantidade = Math.ceil (vida/ataque);
+
+// console.log("A quantidade de turnos necessária para derrotar o monstro é: "+quantidade);
+
+// Exercicio 7
+
+// console.log("E os 10% do garçom? ");
+
+// let valorConta = +prompt(`digite o valor do almoço`);
+
+// let taxa = +prompt(`digite o valor da taxa`);
+
+// let valor = valorConta*taxa;
+
+// console.log("o valor é R$: "+valor.toFixed(2));
+
+//Exercicio 8
+
+// console.log("Qual o valor do troco? ");
+
+// let valorCompra= +prompt(`digite o valor da compra`);
+
+// let valorPago= +prompt(`digite o valor pago`);
+
+// let total= valorCompra-valorPago;
+
+// console.log("o troco é R$: "+total);
+
+//Exercicio 9
+
+// console.log("Você está na flor da idade?");
+
+//   let anoNascimento = +prompt("Digite o ano de seu nascimento.");
+
+//   let anoAtual = +prompt("Digite o ano Atual");
+
+//   let idade = anoAtual-anoNascimento;
+
+//   console.log("Você tem "+ idade + " Anos, Você está na flor da idade.");
+
+//Exercicio 10
+
+// console.log("Conversor de moedas");
+
+// console.log("Cotações para o dia 19/08/2021");
+
+// let valor = +prompt("Digite o valor que deseja converter R$: ");
+// let cotacaoDolar = 5.41;
+// let cotacaoEuro = 6.33;
+// let cotacaoLibra = 7.39;
+// let cotacaoCanada = 4.23;
+// let cotacaoArgentina = 0.056;
+// let cotacaoChile = 0.0069;
+// console.log()
+// console.log(`Valor em R$: ${valor}
+// `);
+// console.log();
+
+// console.log("Valor em Dólar Americano:  US$" +valor/cotacaoDolar);
+// console.log("Valor em Euro: EUR " +valor/cotacaoEuro);
+// console.log("Valor em Libra: GBP " +valor/cotacaoLibra);
+// console.log("Valor em Dólar Canadense: CAD "+valor/cotacaoCanada);
+// console.log("Valor em Peso Argentino: ARP "+valor/cotacaoArgentina);
+// console.log("Valor em Peso Chileno: CLP "+valor/cotacaoChile);
+
+
+//// Exercicio 11 - Calculadora de aumento de aluguel
+// console.log("Calculadora de aumento de aluguel");
+
+// let valorAluguel = +prompt("Digite o Valor do seu aluguel: ");
+
+// let valorDaporcentagem = 31 / 100;
+
+// let aumentoAluguel = valorAluguel * valorDaporcentagem;
+
+// let valortotalalguel = aumentoAluguel + valorAluguel;
+// console.log("R$" + valortotalalgue.toFixed(2)l);
+
+////Exercicio 12
+
+// console.log("Calculadora de aumento de aluguel");
+
+// let valorAluguel = +prompt("Digite o Valor do seu aluguel: ");
+
+// let valorDaporcentagem = prompt("Digite o valor da porcentagem: ")
+// let valorDaporcentagemconv = Number(valorDaporcentagem);
+
+
+
+
+// let aumentoAluguel = valorAluguel * valorDaporcentagem / 100;
+
+// let valortotalalguel = aumentoAluguel + valorAluguel;
+// console.log("R$" + valortotalalguel.toFixed(2));
+
+
---
+++ CODELAB04.js
@@ -0,0 +1,99 @@
+////Exercicio 2 Escreva um programa que solicite uma frase ao usuário e escreva a frase toda em maiúscula e sem espaços em branco
+// const frase = "Os limites só existem se você os deixar existir.(goku)";
+// console.log(frase.lenght);
+// console.log(frase.substring(48));
+
+
+
+
+// let frase = prompt("Digite aqui a sua frase");
+// console.log(frase.toUpperCase())
+
+////Exercicio 3 Elabore um programa que recebe o seu nome, endereço e hobby.
+
+// let dados = [nome = prompt("digite seu nome Completo"), endereço = prompt("digite seu endereço")];
+
+// let hobby = prompt("Digite seu Hobby");
+
+// console.log(dados[0].toUpperCase());
+// console.log(dados[1].toLowerCase());
+// console.log(hobby[0].toUpperCase()+ hobby.substring(1))
+
+////Exercicio 4
+
+// let numero = prompt("Digite um numero");
+// let total = numero/2;
+
+// if(numero & 1){
+//     console.log("Impar");
+// } else {
+//     console.log("Par");
+// }
+
+
+////Exercicio 5
+
+// let numero = prompt("Digite um numero");
+// let total = numero/2;
+
+// if(numero & 1){
+//     console.log("Impar");
+// } else {
+//     console.log("Par");
+// }
+// console.log(total);
+
+//// Exercicio 5/2
+
+// let numero = prompt("Digite um numero: ");
+
+// if(numero > 0){
+//   console.log("O numero é positivo")
+// }else if (numero < 0){
+//   console.log("O numero é negativo")
+// }else{
+//   console.log("Seu numero é invalido")
+// }
+
+////Exercicio 6
+
+// let numero = prompt("Digite um numero: ");
+// let numero2 = prompt("Digite outro numero: ");
+
+// let maior = Math.max(numero, numero2);
+// console.log(maior);
+// if(numero===numero2){
+//   console.log("Numeros iguais")
+// }
+
+////Exercicio 7
+
+// let genero = prompt("Digite uma letra");
+
+// if(genero === "M"){
+//   console.log("Masculino")
+// }else if(genero==="F"){
+//   console.log("Feminino")
+// }else{
+//   console.log("Sexo Inválido")
+// }
+
+
+////Exercicio 8
+
+// let nota = +prompt("Digite sua nota!");
+
+// if(nota < 6.0){
+//   console.log("nota F")
+// }else if(nota <7.0){
+//   console.log("nota D")
+// }else if(nota  <8.0){
+//   console.log("nota C")
+// }else if(nota  <9.0){
+//   console.log("nota B")
+// }else if (nota  <=10.0){
+//   console.log("nota A")}
+// else {}
+
+
+
