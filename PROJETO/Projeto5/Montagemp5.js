3f88fa81331145ec4fcf44b969e1ba94d9aad01d
Pedrocod <pedro.hsoares131@gmail.com>
2021-09-14 22:38:13 -0300

    Codigos do projeto

---
+++ PROJETO/Projeto5/Montagemp5.js
@@ -0,0 +1,327 @@
+// const prompt = require('prompt-sync')();
+
+// class Pessoa {
+//     constructor(dinheiro){
+//         this.cafeTomado = false,
+//         this.dinheiro = dinheiro,
+//         this.sono = 0
+//         this.hora = 8
+//     }
+
+//     tomarcafe(){
+//         console.log("Você tomou um café gostosinho!");
+//         this.cafeTomado = true;
+//     }
+
+//     exibirdados(){
+//         console.log(`
+//         Hoje é o dia 01.
+//         Hora: ${this.hora}
+//         Café tomado: ${this.cafeTomado}
+//         Sono: ${this.sono}
+//         Dinheiro: ${this.dinheiro}
+//         `)
+//     }
+
+//     passarhora(tempo){
+//         this.hora += tempo
+//     }
+
+//     mostrarhora(){
+//         console.log(`Agora são: ${this.hora} horas.`);
+//     }
+// }
+
+
+// var pessoa = new Pessoa(100);
+// pessoa.exibirdados();
+
+// while (true) { 
+//     console.log(`
+//     Escolha uma opção:
+//     1 - Tomar café
+//     2 - Estudar
+//     3 - Trabalhar
+//     4 - Descansar
+//     5 - Status
+//     0 - Sair
+//     `)
+
+//     let opcao = +prompt("Digite sua escolha: ")
+
+//     if (opcao == 1) {
+//         pessoa.tomarcafe();
+//         pessoa.passarhora(1);
+//         pessoa.mostrarhora()
+//     } else if (opcao == 2) {
+//         console.log("Você estudou")
+//         // Aqui eu colocaria o que quero alterar quando ele estuda.
+//         pessoa.passarhora(3)
+//         pessoa.mostrarhora()
+//     } else if (opcao == 3) {
+//         pessoa.mostrarhora()
+//     } else if (opcao == 4) {
+//         pessoa.mostrarhora()
+//     } else if (opcao == 5) {
+   
+//         break
+//     } else if (opcao == 0) {
+//         break
+//     } else {
+//         console.log("Opção inválida!")
+//     }
+// } 
+
+
+
+
+
+
+
+
+// var continuar = 0;
+
+// class Relogio{
+//     constructor(){
+//         this.horas = 16;
+//         this.minutos = 20;
+//         this.dia = 1
+//     }
+//     mostrarHorario(){
+
+//         console.log(`Agora são ${this.horas}:${this.minutos} horas`);
+//     }
+//     avancaTempo(minutos){
+//         this.minutos += minutos
+//         while(this.minutos >= 60){
+//             this.minutos -= 60;
+//             this.horas += 1;
+//         }
+//         while(this.horas >= 24){
+//             this.horas -= 24
+//             this.dia += 1
+//         }
+
+//     }
+// }
+
+
+// do {
+// ////CLASSE QUE SERA RESPONSAVEL PELO PESONAGEM
+
+// class Personagem {
+
+//   constructor(nome, higiene, sair, fase, vidas, hora, minutos, dia, totalVidas) {
+
+//     this.nome = nome;
+//     this.higiene = higiene;
+//     this.sair = false;
+//     this.fase = 0
+//     this.vidas = 5;
+//     this.horas = 8;
+//     this.minutos = 0;
+//     this.dia = 1;
+//     this.totalVidas = 0;
+//   }
+
+//   alterarNome(){
+//   return this.nome
+//   }
+//   sairMascara(){
+//         this.sair  = true;
+//   }
+//   sairFesta(){
+//         this.sair  = false;     
+//   }
+//   sairMercado (){
+//     this.sair = false;
+//   }
+//   visitarVovozinha (){
+//     this.sair = false;
+//   }
+//   higienizar(){
+//     this.higiene = true;
+//   }
+//   totaldeVidas() {
+//     this.vidas = true;
+//     return this.vidas
+//   }
+  
+//   status(){
+//       console.log(`
+//       Hoje é o dia ${this.dia}
+//       Horas: ${this.horas}
+//       Vidas: ${this.vidas}
+//       `)
+//   }
+
+//   avancaTempo(tempo){
+//     this.hora += tempo
+//   }
+
+//   mostrarhora(){
+//       console.log(`Agora são ${this.horas}:${this.minutos} horas`);
+// }
+// }
+
+
+
+// personagem = prompt(`****** Informe o nome do jogador ******:`);
+
+// var personagem = new Personagem (100);
+// personagem.status();
+
+
+
+// higiene();
+
+// var totalVidas = 0;
+// totalVidas = this.vidas;
+
+// function higiene(){
+
+// for (var vidas = 0; vidas += 5; vidas++){
+
+//     console.log(`
+//     Hora de higienizar as mãos!\n
+//     Escolha uma opção:
+//     1 - Lavou com água e sabão
+//     2 - Só passou ácool em gel 70º
+//     3 - Só lavou com água
+//     4 - Não lavou
+//     0 - Sair
+//     `)
+
+//     let opcao = prompt("Digite sua escolha:");
+    
+//     if (opcao == 1) {
+//         console.log("Lave as mãos várias vezes ao dia, ganhou 4 vidas.");
+//         console.log();
+//         personagem.avancaTempo(1);
+//         personagem.mostrarhora()
+//         console.log(`Você tem ${vidas += 4} vidas`);
+//         return;
+
+//     } else if (opcao == 2) {
+//         console.log("O uso do álcool gel é para sujeiras não visíveis.\nGanhou 3 vidas.");
+//         personagem.mostrarhora();
+//         console.log(`Você tem ${vidas += 3} vidas`);
+//         break
+
+//     } else if (opcao == 3) {
+//         console.log("Só água não mata os vírus. Perdeu 4 vidas")
+//         personagem.mostrarhora();
+//         console.log(`Você tem ${vidas -= 4} vidas`);
+//         break
+
+//     } else if (opcao == 4) {
+//       console.log("Lave antes e depois de ir ao banheiro, quando for manusear alimentos, quando tocar em algo, quando sair e voltar de casa. Procedimentos que devem ser habituais e não só para este momento. Perdeu 5 vidas!");
+//         personagem.mostrarhora();
+//         console.log(`Você tem ${vidas -=5 } vidas`);
+//         break
+
+//     } else if (opcao == 0) {
+//         break
+//     } else {
+//         console.log("Opção inválida!")
+//     }
+// } 
+// }
+
+
+
+// var fase = prompt("Deseja seguir para 2ª fase? S/N: ");
+
+// while(fase == "N"){
+
+//   console.log(this.vidas())
+//   console.log(`Vidas já conquistadas `);
+//   console.log("Fim de jogoto");
+//   break
+  
+// }
+
+// trabalhar();
+
+// console.log("O distanciamento e o isolamento social são provisórios e necessários para reduzir a propagação do vírus. Mantenha distância mínima de um metro e meio entre pessoas em lugares públicos e de convívio social, sempre usando máscaras.\n");
+
+
+
+// function trabalhar(){
+
+// let trabalho = prompt("Vai sair de casa? S/N")
+    
+// if(trabalho.toUpperCase() == "N"){
+
+//       console.log(`Você ganhou ${vidas == 5} vidas`);
+// } else {while (trabalho == "S"){
+
+//     console.log(`
+//     Escolha uma opção:
+//     1 - Tá com máscara?
+//     2 - Ônibus
+//     3 - Vai para Festa?
+//     4 - Vai para o Bar
+//     5 - Ida ao Mercado?
+//     6 - Visitar a Vovozinha 
+//     7 - Status
+//     0 - Sair
+//     `)
+
+//    let opcao = +prompt("Digite sua escolha: ");
+
+
+//     if (opcao == 1) {
+//         console.log("Parabés! Como você tá seguindo o protocolo, ganhou 5 vidas.");
+//         personagem.avancaTempo(1);
+//         personagem.mostrarhora()
+//         console.log(`Você tem ${vidas += 5} vidas`);
+
+//     } else if (opcao == 2) {
+        
+//         console.log("Você escolheu ir de Ônibus. Foi contaminado e contaminou mais 100 pessoas, perdeu 5 vidas");
+//         personagem.mostrarhora()
+//         console.log(`Você tem ${vidas -= 5} vidas`);
+
+//     } else if (opcao == 3) {
+//         console.log("Você foi irresponsável, perdeu 4 vidas");
+//         personagem.mostrarhora();
+//         console.log(`Você tem ${vidas -= 4} vidas`);
+        
+
+//     } else if (opcao == 4) {
+//         console.log("Você foi irresponsável, perdeu 4 vidas")
+//         personagem.mostrarhora();
+//         console.log(`Você tem ${vidas -= 4} vidas`);
+      
+
+//     } else if (opcao == 5) {
+//       console.log("Você ainda não aprendeu que nesse momento precisamos manter o isolamento social, online é a melhor opção para compras. Como foi sozinho para o Mercado, só perdeu 2 vidas");
+//         personagem.mostrarhora();
+//         console.log(`Você tem ${vidas -= 2} vidas`);
+
+//     } else if (opcao == 6) {
+//         console.log("Você foi irresponsáve!\nÉ muito importante manter o isolamento social.\nAme a Vida!!!\n.Perdeu 5 vidas")
+//         personagem.mostrarhora();
+//         console.log(`Você tem ${vidas -= 5} vidas`);
+//       break
+//     } else if (opcao == 7) {
+//         pessoa.status();
+    
+//     } else if (opcao == 0) {
+//         personagem.status();
+//         break;
+
+//     } else {
+//         console.log("Opção inválida!")
+//     }
+// } 
+// }
+
+
+
+// }
+
+// continuar = prompt("Fim de jogo. Deseja continuar? (Resp. S / N):").toUpperCase()
+
+// }while(continuar === "S")
---
+++ PROJETO/Projeto5/Projeto5.js
@@ -0,0 +1,221 @@
+const { SSL_OP_NO_COMPRESSION } = require('constants');
+const { get } = require('prompt');
+const prompt = require('prompt-sync')();
+// // CRIANDO UMA CLASSE PARA O RELOGIO // //
+function sleep(n) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
+class Relogio{
+    constructor(){
+        this.Horas = 16;
+        this.Minutos = 20;
+        this.dia = 1
+    }
+    mostrarHorario(){
+        
+        console.log(`Agora são ${this.Horas}:${this.Minutos} do ${this.dia} Dia`)
+    }
+    avancaTempo(Minutos){
+        this.Minutos += Minutos
+        while(this.Minutos >= 60){
+            this.Minutos -= 60;
+            this.Horas += 1;
+        }
+        while(this.Horas >= 24){
+            this.Horas -= 24
+            this.dia += 1
+        }    
+
+    }
+}
+// // Criando a Classe para o personagem // // 
+class Personagem {
+    constructor(){
+        this.cansado = false;
+        this.sujo = false;
+        this.fome = false;
+    
+    }
+    limparcasa(){
+        console.log("Você escolheu limpar a casa");
+        this.cansado = true;
+        this.sujo = true;
+        this.fome = true;
+        console.log();
+        console.log("Você demorou 3 horas para limpar a casa");
+        horario.avancaTempo(180);
+        if (this.cansado == true){
+            console.log("Você esta cansado demais para limpar a casa, va fazer outra coisa.");
+        }
+    }
+    
+    tomarBanho(){
+       if(this.sujo === true){
+            console.log("Você esta tomando banho");
+            this.sujo = false;
+            sleep(1500);
+            console.log("Você ficou 30 minutos tomando banho e esta de banho tomado!");
+            horario.avancaTempo(30);
+        }else{
+            console.log("Você não pode tomar banho pois ja esta limpo.")
+        }
+    }
+    comer(){
+        if(this.fome === true){
+            console.log("Você preparou algo gostoso e comeu tudo!");
+            this.fome = false;
+            horario.avancaTempo(40);
+        }else{
+            console.log("Você esta Cheio, Não pode comer mais!!");
+        }
+    }
+    dormir(){
+        console.log(`Você foi dormir as ${horario.Horas}:${horario.Minutos}`);
+        if(this.cansado === true){
+            this.cansado = false;
+            this.fome = true;
+            this.sujo = true;
+            horario.Horas = 10;
+            horario.Minutos = 20;
+            console.log();
+            horario.mostrarHorario();
+            
+        }else {
+            console.log("Você não estava cansado o suficiente então, dormiu por 4 horas");
+            horario.avancaTempo(240);
+            horario.mostrarHorario();
+        }
+}
+    informacoes(){
+        console.log(`
+        Hoje é o Dia ${horario.dia}
+        Horario: ${horario.Horas}:${horario.Minutos}
+        Cansado: ${this.cansado}
+        Fome: ${this.fome}
+        Sujo: ${this.sujo}
+        `);
+
+    }
+}
+// Atribuindo as duas classes a uma variavel//
+var horario = new Relogio();
+
+var personagem = new Personagem();
+console.log(`                   Corra pela sua vida
+    Você decide passar as férias acampados em uma cabana na floresta, mas mal sabia o que o esperava.
+Existia um Serial killer desconhecido que vivia naquela floresta, esse serial killer ira perturbar
+a sua paz, faça as escolhas certas para sobreviver ou morra tentando escapar daquele local.
+`);
+
+console.log();
+
+console.log("Após um dia longo de viagem finalmente cheguei na cabana dos meus pais, o que fazer agora ? ");
+
+console.log();
+// // Inicio do jogo que sempre recebera true pois não existe um prompt // // 
+while(true){
+    personagem.informacoes();
+    console.log(`
+    Selecione uma opção:
+    1 - Limpar a cabana, arrumar tudo;
+    2 - Comer comida
+    3 - Tomar banho e escovar os dentes;
+    4 - Assistir TV
+    5 - Se preparar e ir dormir
+    6 - Sair para dar uma volta na floresta
+    0 - Desistir
+    `);
+    
+    let escolha = +prompt("Faça a sua escolha: ");
+    
+    if(escolha == 1){
+        console.clear();
+        personagem.limparcasa();
+        horario.mostrarHorario();
+    }
+    else if(escolha == 2){
+        console.clear();
+        personagem.comer();
+        horario.mostrarHorario();
+    }
+    else if(escolha == 3){
+        console.clear()
+        personagem.tomarBanho();
+        horario.mostrarHorario();
+    }
+    else if(escolha == 4){
+        console.clear();
+        horario.avancaTempo(60);
+        if((horario.Horas <= 23) || (horario.Horas >= 06)){
+            console.log("Você liga a TV para assistir, e se depara com a noticia de que existe um Assassino a solta na sua região \nvocê fica assustado com a noticia e tranca a casa toda no primeiro dia.");
+            personagem.medo = true;
+            horario.avancaTempo(60);
+        }else if((horario.Horas >= 23) || (horario.Horas < 06)){
+            console.log("Você passou do horario de dormir, apague as luzes ou o assassino ira atras de você");
+            personagem.medo = true;
+        }
+    }
+    else if (escolha == 5){
+        console.clear();
+        personagem.dormir();
+        personagem.medo = false
+    }
+    else if (escolha == 6){
+        if(personagem.medo == true){
+        console.log("Você pensa em sair para andar pela floresta mas lembra do Assassino que tem a Solta na sua região.")
+        }else{    
+        console.log("Você saiu para caminhar na floresta e ficou algumas horas fora");
+        console.clear();
+        horario.avancaTempo(240);
+        console.log();
+        if((horario.Horas >= 23) || (horario.Horas <= 06)){
+            horario.mostrarHorario()
+            console.log();
+            console.log("Você saiu pra caminhar muito tarde e escutou barulhos estranhos na floresta");
+            personagem.medo = true
+        }if (personagem.medo == true){
+            console.log();
+            console.log("Você encontrou o Assassino na floresta e ele começou a te perseguir");
+            sleep(500)
+            console.log(`
+            selecione uma opção
+            1 Correr
+            2 Tentar enfrentrar o Assassino`);
+            let seleciona = +prompt("Qual a sua escolha? ");
+            if(seleciona == 1 && personagem.cansado == true){
+                console.log("Voce tentou correr do Assassino mas estava cansado e acabou caindo \n ele te pegou e te matou.");
+                sleep(500);
+                console.log(`Você morreu as ${horario.Horas}:${horario.Minutos} do dia ${horario.dia}`);
+                console.log();
+                break
+            }else if(seleciona == 1 && personagem.cansado == false){
+                console.log("Você tentou correr do Assassino");
+                console.log();
+                sleep(500);
+                console.log("Você fugiu com sucesso do Assassino e trancou toda a Cabana.");
+                personagem.cansado = true
+            }else if(seleciona == 2 && personagem.cansado == true){
+                console.log("Você tentou lutar contra o Assassino.");
+                console.log();
+                sleep(500);
+                console.log("Você perdeu a luta para o Assassino e ele te matou.");
+                sleep(500);
+                console.log(`Você morreu as ${horario.Horas}:${horario.Minutos} do dia ${horario.dia}`);
+                console.log();
+                break
+            }else if(seleciona == 2 && personagem.cansado == false){
+                console.log("Você tentou lutar contra o Assassino");
+                console.log();
+                sleep(500);
+                console.log("Você estava bem e conseguiu derrubar o assassino, Você conseguiu fugir e trancou toda a cabana.");
+                personagem.cansado = true;
+            }
+            
+        }
+    }
+    }else if (escolha == 0){
+        console.log("Você desistiu do jogo");
+        break
+    }
+}
+// // Fim do programa que roda todo o jogo de escolhas // // 
